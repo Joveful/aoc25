e4610ce852e130f892bb065091a6302b9c6eb0dd
Jori Vehkakoski <j.vehkakoski@gmail.com>
2026-04-18 15:06:06 +0300

    day 3 part 2

--- day03/day03.cpp
+++ day03/day03.cpp
@@ -23,31 +23,29 @@ std::string loadData(const std::string& filepath) {
         | std::views::transform([](auto r) { return std::string_view(r); });
 }
 
-int solvePart1(const std::string& input) {
-    std::array<int, 2> jolts{0, 0};
-    int totalJolts = 0;
+long long solve(const std::string& input, int n) {
+    long long joltage = 0;
+    long long totalJolts = 0;
+    int highest = 0;
+    int currentIndex = 0;
     auto rows = lines(input);
 
-    auto highestJolt = [&](char c, std::array<int, 2>& jolts) {
-        if (jolts[0] < c - '0') {
-            jolts[0] = c - '0';
-            jolts[1] = 0;
-        } else if (jolts[1] < c - '0') {
-            jolts[1] = c - '0';
-        }
-    };
-
     for (auto row : rows) {
-        std::ranges::for_each(row | std::views::take(row.size() - 1), [&](char c) {
-            highestJolt(c, jolts);
-        });
-        char lastChar = row.back();
-        if (jolts[1] < lastChar - '0')
-            jolts[1] = lastChar - '0';
-
-        totalJolts += 10 * jolts[0] + jolts[1];
-        jolts[0] = 0;
-        jolts[1] = 0;
+        for (int i = 0; i < n; ++i) {
+            int j = currentIndex;
+            std::ranges::for_each(std::views::counted(row.begin() + currentIndex, row.size() - (n - 1) + i - currentIndex), [&](char c) {
+                j += 1;
+                if (c - '0' > highest) {
+                    highest = c - '0';
+                    currentIndex = j;
+                }
+            });
+            joltage += std::pow(10, n - 1 - i) * highest;
+            highest = 0;
+        }
+        totalJolts += joltage;
+        currentIndex = 0;
+        joltage = 0;
     }
     return totalJolts;
 }
@@ -60,6 +58,8 @@ int main(int argc, const char** argv) {
     std::string path = std::string(INPUT_DIR) + "/" + argv[1];
     std::string input;
     input = loadData(path);
-    int result = solvePart1(input);
+    // Part 1
+    // long long result = solve(input, 2);
+    long long result = solve(input, 12);
     std::cout << "Solution: " << result << '\n';
 }
